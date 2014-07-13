32c8d242b5e946b3c10447fa2426c062a738c509
Paul Jensen <paulbjensen@gmail.com>
2014-07-13 09:35:32 +0100

    more WIP

--- test/dashku.js
+++ test/dashku.js
@@ -259,7 +259,7 @@ describe('Dashku', function () {
 						name:         'My little widgie',
 						html:         '<div id=\'bigNumber\'></div>',
 						css:          '#bigNumber {\n  padding: 10px;\n  margin-top: 50px;\n  font-size: 36pt;\n  font-weight: bold;\n}',
-						script:       "// The widget's html as a jQuery object\nvar widget = this.widget;\n\n// This runs when the widget is loaded\nthis.on('load', function(data){\n  console.log('loaded');\n});\n// This runs when the widget receives a transmission\nthis.on('transmission', function(data){\n  widget.find('#bigNumber').text(data.bigNumber);\n});",
+						script:       '// The widget\'s html as a jQuery object\nvar widget = this.widget;\n\n// This runs when the widget is loaded\nthis.on(\'load\', function(data){\n  console.log(\'loaded\');\n});\n// This runs when the widget receives a transmission\nthis.on(\'transmission\', function(data){\n  widget.find(\'#bigNumber\').text(data.bigNumber);\n});',
 						json:         '{\n  "bigNumber":500\n}'
 					};
 
@@ -285,7 +285,7 @@ describe('Dashku', function () {
 					name:         'My little widgie',
 					html:         '<div id=\'bigNumber\'></div>',
 					css:          '#bigNumber {\n  padding: 10px;\n  margin-top: 50px;\n  font-size: 36pt;\n  font-weight: bold;\n}',
-					script:       "// The widget's html as a jQuery object\nvar widget = this.widget;\n\n// This runs when the widget is loaded\nthis.on('load', function(data){\n  console.log('loaded');\n});\n// This runs when the widget receives a transmission\nthis.on('transmission', function(data){\n  widget.find('#bigNumber').text(data.bigNumber);\n});",
+					script:       '// The widget\'s html as a jQuery object\nvar widget = this.widget;\n\n// This runs when the widget is loaded\nthis.on(\'load\', function(data){\n  console.log(\'loaded\');\n});\n// This runs when the widget receives a transmission\nthis.on(\'transmission\', function(data){\n  widget.find(\'#bigNumber\').text(data.bigNumber);\n});',
 					json:         '{\n  "bigNumber":500\n}'
 
 				};
@@ -317,7 +317,7 @@ describe('Dashku', function () {
 						name:         'My little widgie',
 						html:         '<div id=\'bigNumber\'></div>',
 						css:          '#bigNumber {\n  padding: 10px;\n  margin-top: 50px;\n  font-size: 36pt;\n  font-weight: bold;\n}',
-						script:       "// The widget's html as a jQuery object\nvar widget = this.widget;\n\n// This runs when the widget is loaded\nthis.on('load', function(data){\n  console.log('loaded');\n});\n// This runs when the widget receives a transmission\nthis.on('transmission', function(data){\n  widget.find('#bigNumber').text(data.bigNumber);\n});",
+						script:       '// The widget\'s html as a jQuery object\nvar widget = this.widget;\n\n// This runs when the widget is loaded\nthis.on(\'load\', function(data){\n  console.log(\'loaded\');\n});\n// This runs when the widget receives a transmission\nthis.on(\'transmission\', function(data){\n  widget.find(\'#bigNumber\').text(data.bigNumber);\n});',
 						json:         '{\n  "bigNumber":500\n}'
 					};
 
@@ -360,7 +360,7 @@ describe('Dashku', function () {
 						name:         'My little widgie',
 						html:         '<div id=\'bigNumber\'></div>',
 						css:          '#bigNumber {\n  padding: 10px;\n  margin-top: 50px;\n  font-size: 36pt;\n  font-weight: bold;\n}',
-						script:       "// The widget's html as a jQuery object\nvar widget = this.widget;\n\n// This runs when the widget is loaded\nthis.on('load', function(data){\n  console.log('loaded');\n});\n// This runs when the widget receives a transmission\nthis.on('transmission', function(data){\n  widget.find('#bigNumber').text(data.bigNumber);\n});",
+						script:       '// The widget\'s html as a jQuery object\nvar widget = this.widget;\n\n// This runs when the widget is loaded\nthis.on(\'load\', function(data){\n  console.log(\'loaded\');\n});\n// This runs when the widget receives a transmission\nthis.on(\'transmission\', function(data){\n  widget.find(\'#bigNumber\').text(data.bigNumber);\n});',
 						json:         '{\n  "bigNumber":500\n}'
 					};
 
@@ -402,7 +402,7 @@ describe('Dashku', function () {
 						name:         'My little widgie',
 						html:         '<div id=\'bigNumber\'></div>',
 						css:          '#bigNumber {\n  padding: 10px;\n  margin-top: 50px;\n  font-size: 36pt;\n  font-weight: bold;\n}',
-						script:       "// The widget's html as a jQuery object\nvar widget = this.widget;\n\n// This runs when the widget is loaded\nthis.on('load', function(data){\n  console.log('loaded');\n});\n// This runs when the widget receives a transmission\nthis.on('transmission', function(data){\n  widget.find('#bigNumber').text(data.bigNumber);\n});",
+						script:       '// The widget\'s html as a jQuery object\nvar widget = this.widget;\n\n// This runs when the widget is loaded\nthis.on(\'load\', function(data){\n  console.log(\'loaded\');\n});\n// This runs when the widget receives a transmission\nthis.on(\'transmission\', function(data){\n  widget.find(\'#bigNumber\').text(data.bigNumber);\n});',
 						json:         '{\n  "bigNumber":500\n}'
 					};
 
@@ -433,7 +433,7 @@ describe('Dashku', function () {
 						name:         'My little widgie',
 						html:         '<div id=\'bigNumber\'></div>',
 						css:          '#bigNumber {\n  padding: 10px;\n  margin-top: 50px;\n  font-size: 36pt;\n  font-weight: bold;\n}',
-						script:       "// The widget's html as a jQuery object\nvar widget = this.widget;\n\n// This runs when the widget is loaded\nthis.on('load', function(data){\n  console.log('loaded');\n});\n// This runs when the widget receives a transmission\nthis.on('transmission', function(data){\n  widget.find('#bigNumber').text(data.bigNumber);\n});",
+						script:       '// The widget\'s html as a jQuery object\nvar widget = this.widget;\n\n// This runs when the widget is loaded\nthis.on(\'load\', function(data){\n  console.log(\'loaded\');\n});\n// This runs when the widget receives a transmission\nthis.on(\'transmission\', function(data){\n  widget.find(\'#bigNumber\').text(data.bigNumber);\n});',
 						json:         '{\n  "bigNumber":500\n}'
 					};
 
